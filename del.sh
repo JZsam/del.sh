fa2fccf43c6ebbb8b9360951bce81426c6c03c23
Jzdoot <jacobsamurin@gmail.com>
2021-11-26 11:49:23 -0500

    updated del.sh to beta 0.1.0 | added the the ablility to change the amount of images you want

--- del.sh
+++ del.sh
@@ -1,14 +1,66 @@
 #! /usr/bin/env bash
 set -euo pipefail
+####################
+# FUNCTIONS        #
+####################
+Help(){
+	echo
+	echo "del.sh [-h|v|l] mod"
+	echo "options:"
+	echo "h		Print this help statment"
+	echo "v		Prints the version"
+	echo "l		Changes the standard (take the lager side like 2/3 or 3/4) to take 1/3 or 1/4"
+}
+####################
+# SETTING VARIABLES#
+####################
+Lower=false
+####################
+# OPTIONS          #
+####################
+while getopts ":hvl" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+	 v)
+		echo beta 0.1.0
+		 exit;;
+	 l)
+		 Lower=true;;
+		 # exit;;
+	 \?)
+		 echo "Error: Invalid option"
+		 exit;;
+   esac
+done
+####################
+# MAIN			   #
+####################
+# echo $Lower
+# echo $1
+# echo $2
 list=$( ls | sed 's/.jpg//g' | sed '/del.sh/d' | xargs)
 echo $list
 mkdir "$(pwd)_del/"
-for current in $list; do
-	new=$(($current%2))
-	if [ $new -eq 1 ] ; then
-		mv "$current.jpg" "$(pwd)_del/"
-		echo "odd;next"
-	else
-		echo "Even; next"
-	fi
-done
+if [ $Lower == false ]; then
+	for current in $list; do
+		new=$(($current%$1))
+		if [ $new != 0 ] ; then
+			mv "$current.jpg" "$(pwd)_del/"
+			echo "Move;next"
+		else
+			echo "Stay; next"
+		fi
+	done
+elif [ $Lower == true ];  then
+	for current in $list; do
+		new=$(($current%$2))
+		if [ $new = 0 ] ; then
+			mv "$current.jpg" "$(pwd)_del/"
+			echo "Move;next"
+		else
+			echo "Stay; next"
+		fi
+	done
+fi
