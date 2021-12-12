7ff8fc0a8ab726e3e55acebad85faceda8ad210f
Jzdoot <jacobsamurin@gmail.com>
2021-12-12 10:13:11 -0500

    tryed adding a path feature

--- del.sh
+++ del.sh
@@ -9,30 +9,35 @@ Help(){
 	echo "options:"
 	echo "h		Print this help statment"
 	echo "v		Prints the version"
-	echo "l		Changes the standard (take the lager side like 2/3 or 3/4) to take 1/3 or 1/4"
+	echo "l		Changes the standard (take the larger side like 2/3 or 3/4) to take 1/3 or 1/4"
 }
 ####################
 # SETTING VARIABLES#
 ####################
 Lower=false
+Path=$(pwd)
+PathChange=false
 ####################
 # OPTIONS          #
 ####################
-while getopts ":hvl" option; do
-   case $option in
-      h) # display Help
-         Help
-         exit;;
-	 v)
-		echo beta 0.1.0
-		 exit;;
-	 l)
-		 Lower=true;;
-		 # exit;;
-	 \?)
-		 echo "Error: Invalid option"
-		 exit;;
-   esac
+while getopts ":hvlp" option; do
+	case $option in
+		h) # display Help
+			Help
+			exit;;
+		v)
+			# echo beta 0.1.0
+			echo alpha 0.1.1
+			exit;;
+		l)
+			Lower=true;;
+		p)
+			path=$OPTARG
+			PathChange=true;;
+		\?)
+			echo "Error: Invalid option"
+			jexit;;
+	esac
 done
 ####################
 # MAIN			   #
@@ -40,27 +45,56 @@ done
 # echo $Lower
 # echo $1
 # echo $2
-list=$( ls | sed 's/.jpg//g' | sed '/del.sh/d' | xargs)
-echo $list
-mkdir "$(pwd)_del/"
-if [ $Lower == false ]; then
-	for current in $list; do
-		new=$(($current%$1))
-		if [ $new != 0 ] ; then
-			mv "$current.jpg" "$(pwd)_del/"
-			echo "Move;next"
-		else
-			echo "Stay; next"
-		fi
-	done
-elif [ $Lower == true ];  then
-	for current in $list; do
-		new=$(($current%$2))
-		if [ $new = 0 ] ; then
-			mv "$current.jpg" "$(pwd)_del/"
-			echo "Move;next"
-		else
-			echo "Stay; next"
-		fi
-	done
+if [ $PathChange ]; then
+	list=$( ls | sed 's/.jpg//g' | sed '/del.sh/d' | xargs)
+	echo $list
+	mkdir "$(pwd)_del/"
+	if [ $Lower == false ]; then
+		for current in $list; do
+			new=$(($current%$1))
+			if [ $new != 0 ] ; then
+				mv "$current.jpg" "$(pwd)_del/"
+				echo "Move;next"
+			else
+				echo "Stay; next"
+			fi
+		done
+	elif [ $Lower == true ];  then
+		for current in $list; do
+			new=$(($current%$2))
+			if [ $new = 0 ] ; then
+				mv "$current.jpg" "$(pwd)_del/"
+				echo "Move;next"
+			else
+				echo "Stay; next"
+			fi
+		done
+	fi
+else
+	cd $Path
+	list=$( ls | sed 's/.jpg//g' | sed '/del.sh/d' | xargs)
+	echo $list
+	mkdir "$(pwd)_del/"
+	if [ $Lower == false ]; then
+		for current in $list; do
+			new=$(($current%$1))
+			if [ $new != 0 ] ; then
+				mv "$current.jpg" "$(pwd)_del/"
+				echo "Move;next"
+			else
+				echo "Stay; next"
+			fi
+		done
+	elif [ $Lower == true ];  then
+		for current in $list; do
+			new=$(($current%$2))
+			if [ $new = 0 ] ; then
+				mv "$current.jpg" "$(pwd)_del/"
+				echo "Move;next"
+			else
+				echo "Stay; next"
+			fi
+		done
+	fi
+	cd -
 fi
